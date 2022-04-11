38a2c0ae6072b24e155964c41abcb295e1237982
MrSkit <91326600+MrSkitson@users.noreply.github.com>
2022-04-11 12:30:59 +0200

    PlayerController
    Add Player and PlayerController script to movement player

---
+++ Doors/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Movement")]
+    [SerializeField] Transform playerCamera = null;
+    public float moveSpeed = 6f;
+    float movementMultiplier = 10f;
+
+    float rbDrag = 6;
+    float horizontalMovement;
+    float verticalMovement;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {  
+        MyInput();
+        ControlDrag();
+    }
+
+    void MyInput()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+
+        moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
+    }
+
+    void ControlDrag()
+    {
+        rb.drag = rbDrag;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+    }
+
+}
