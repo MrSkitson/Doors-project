53a5b8a8abc7af96f700c5e930d8495ba96f79de
MrSkit <91326600+MrSkitson@users.noreply.github.com>
2022-04-11 17:51:33 +0200

    Add playerController asset
    add PlayerController Asset with audio

--- Doors/Assets/Scripts/PlayerController.cs
+++ Doors/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    float playerHeight = 2f;
     [Header("Movement")]
     [SerializeField] Transform playerCamera = null;
     public float moveSpeed = 6f;
     float movementMultiplier = 10f;
-
+    public bool isGrounded;
     float rbDrag = 6;
     float horizontalMovement;
     float verticalMovement;
-
+    
     Vector3 moveDirection;
 
     Rigidbody rb;
@@ -29,8 +29,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {  
+        isGrounded = Physics.Raycast(transform.position, Vector3.right, playerHeight / 2 + 0.1f);
+    
         MyInput();
         ControlDrag();
+        //RotatePlayer();
     }
 
     void MyInput()
@@ -54,6 +57,16 @@ public class PlayerController : MonoBehaviour
     void MovePlayer()
     {
         rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+    }
+
+    void RotatePlayer()
+    {
+        var speed = 0.01f;
+        if (Input.GetKey(KeyCode.Q))
+      transform.Rotate(Vector3.up * speed * Time.deltaTime);
+      
+  if (Input.GetKey(KeyCode.E))
+      transform.Rotate(-Vector3.up * speed * Time.deltaTime);
     }
 
 }
